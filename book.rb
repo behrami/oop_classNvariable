dd023a4744436fcac1a06c0b167055301fc7d136
behrami <behram1993@gmail.com>
2018-03-27 14:58:18 -0400

    kinda done with book

--- book.rb
+++ book.rb
@@ -10,7 +10,7 @@ class Book
   end
 
   def due_date
-    return due_date
+    return @due_date
   end
 
   def due_date=(due_date)
@@ -18,20 +18,34 @@ class Book
   end
 
   def borrow
-
+    if lend_out? == false
+      @due_date=Book.current_due_date
+      loaning=@@on_shelf.delete(self)############################  help me
+      @@on_loan.push(loaning)
+      return true
+    else
+      return false
+    end
   end
 
   def return_to_library
-
+    if lend_out?
+      @due_date= nil
+      depositing=@@on_loan.delete(self)############################  help me
+      @@on_shelf.push(depositing)
+      return true
+    else
+      return false
+    end
   end
 
-  def lent_out
-    @@on_shelf.each do |book|
-      if isbn == book.isbn
-        return true
-      end
+  def lend_out?
+    if @@on_shelf.include?(self)
+      return false
+    else
+      return true
     end
-    return false
+
   end
 
   def isbn
@@ -50,6 +64,11 @@ class Book
 
   def self.overdue_books
 
+    @@on_loan.each do |bookOBJ|
+      if bookOBJ.due_date < Time.now
+        return bookOBJ
+      end
+    end
   end
 
   def self.browse
@@ -72,13 +91,40 @@ newbook2=Book.create("guuci gang2", "lil pump2", "420b2")
 p newbook2
 newbook3=Book.create("guuci gang3", "lil pump3", "420b3")
 p newbook3
+puts
 
-p Book.available
+p Book.available #list all available books in library
+puts
 
-p newbook2.borrowed
+p newbook2.borrowed#returns all the books that were borrowed aka not in library
+puts
 
-p Book.browse
+p Book.browse#list random book in library
+puts
 
-p newbook2.lent_out
+p newbook3.lend_out? #returns true if the books is not in library false if it is in library
+puts
 
-p Book.current_due_date
+p newbook3.borrow#borrow book3
+puts
+
+p newbook3.lend_out?
+puts
+
+# p Book.current_due_date
+# puts
+# p newbook3.borrow
+# puts
+# p newbook3.borrow
+# puts
+p newbook3.due_date
+puts
+p newbook3.borrowed
+puts
+
+# p newbook3.return_to_library
+# puts
+# p newbook3.return_to_library
+# puts
+p Book.overdue_books
+puts
